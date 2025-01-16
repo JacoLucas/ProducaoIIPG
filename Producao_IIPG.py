ce24b972dc4a7e599546e566c206cead1a36ff49
Lucas jacomel <lucas.jacomel@pucpr.edu.br>
2025-01-16 12:22:04 -0300

    Update Producao_IIPG.py

--- Producao_IIPG.py
+++ Producao_IIPG.py
@@ -211,4 +211,5 @@ def update_charts(selected_month, selected_unit):
     return fig_line, fig_bar, fig_pie, fig_line2, fig_usa_uss
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
