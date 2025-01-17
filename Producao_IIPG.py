6e46eede987ed02b6e8bfbf482c5565d582f8761
Lucas jacomel <lucas.jacomel@pucpr.edu.br>
2025-01-17 14:22:50 -0300

    Update Producao_IIPG.py

--- Producao_IIPG.py
+++ Producao_IIPG.py
@@ -17,9 +17,6 @@ if response.status_code == 200:
 else:
     raise Exception("Falha ao baixar o arquivo do GitHub. Verifique a URL e tente novamente.")
 
-# Verifique os nomes das colunas
-print(df.columns)
-
 # Remova espaços em branco dos nomes das colunas
 df.columns = df.columns.str.strip()
 
@@ -29,6 +26,13 @@ df['Dias'] = pd.to_datetime(df['Dias'], format='%d/%m/%Y')  # Ajuste o formato d
 # Extrai ano e mês dos dados
 df['Ano_Mes'] = df['Dias'].dt.to_period('M')
 
+# Calcula e cria coluna de Médias sem contar produção=0
+def media_sem_zeros(grupo):
+    media = grupo[grupo != 0].mean() if grupo[grupo != 0].size > 0 else 0
+    return pd.Series([media]*len(grupo), index=grupo.index)
+
+df['Produção Média'] = df.groupby('Ano_Mes')['Produção Primário'].transform(media_sem_zeros)
+
 # Renomeia as colunas para remover a palavra 'Produção'
 df.rename(columns={
     'Produção CBUQ': 'CBUQ',
@@ -37,12 +41,20 @@ df.rename(columns={
     'Produção BGTC': 'BGTC'
 }, inplace=True)
 
+# Verifique os nomes das colunas
+print(df.columns)
+
 # Inicializa o aplicativo Dash
 app = dash.Dash(__name__)
 app.title = 'Produção IIPG'
 
 app.layout = html.Div([
     html.H1('Produção IIPG - Inst. Ind. Ponta Grossa'),
+
+    ######### ATUALIZAR SEMPRE #########
+    html.H3('Atualizado dia 17/01/25 - 12:04'), 
+    ######### ATUALIZAR SEMPRE #########
+    
     html.Div([
         html.Label('Selecione o Período:'),
         dcc.Dropdown(
@@ -53,7 +65,8 @@ app.layout = html.Div([
     ], style={'width': '33%', 'display': 'inline-block', 'margin-bottom': '20px'}),
     html.Div([
         html.H2('Sistema Primário - Britagem'),
-        dcc.Graph(id='line-chart', style={'width': '100%', 'display': 'inline-block'})
+        dcc.Graph(id='line-chart', style={'width': '100%', 'display': 'inline-block'}),
+        dcc.Graph(id='acum-chart', style={'width': '94%', 'display': 'inline-block'})
     ]),
     html.Div([
         html.H2('Sistema Secundário - Rebritagem'),
@@ -83,6 +96,7 @@ app.layout = html.Div([
 
 @app.callback(
     [Output('line-chart', 'figure'),
+     Output('acum-chart', 'figure'),
      Output('bar-chart', 'figure'),
      Output('pie-chart', 'figure'),
      Output('line2-chart', 'figure'),
@@ -94,6 +108,10 @@ def update_charts(selected_month, selected_unit):
     filtered_df = df[df['Ano_Mes'] == selected_month]
     
     # Mapa de cores
+    colors = {
+        'Produção Primário': '#006699',
+        'Produção Média': '#CC0000'
+    }
     color_map = {
         'Pó de Pedra': '#006699',
         'Pedrisco': '#660099',
@@ -105,16 +123,32 @@ def update_charts(selected_month, selected_unit):
     fig_line = px.line(
         filtered_df, 
         x='Dias', 
-        y='Produção Primário', 
+        y=['Produção Primário', 'Produção Média'],
         title=f'Produção Diária - {selected_month}', 
-        line_shape='linear'
+        line_shape='linear',
+        color_discrete_map=colors,
+        hover_data={'Obs Primário': True}
     )
-    fig_line.update_traces(line=dict(color='#006699'))
     fig_line.update_layout(
         xaxis_title='Período',
-        yaxis_title='Produção (ton.)'
+        yaxis_title='Produção (ton.)',
+        legend_title='Legenda'
     )
     
+    # Gráfico de linha Acumulado Primário
+    fig_acum = px.line(
+        filtered_df,
+        x='Dias',
+        y='Produção Acumulada',
+        title=f'Produção Acumulada - {selected_month}',
+        line_shape='linear'
+    )
+    fig_acum.update_traces(line=dict(color='#FFCC00'))
+    fig_acum.update_layout(
+        xaxis_title='Período',
+        yaxis_title='Produção Acumulada (ton.)'
+    )
+
     # Gráfico de barras horizontais
     data = {
         'Material': ['Pó de Pedra', 'Pedrisco', 'Brita 1', 'Brita 2'],
@@ -153,10 +187,10 @@ def update_charts(selected_month, selected_unit):
     )
 
     # Criando o dataframe line2_df
-    line2_df = pd.concat([filtered_df['Dias'], df[['Pó de Pedra', 'Pedrisco', 'Brita 1', 'Brita 2']]], axis=1)
+    line2_df = pd.concat([filtered_df['Dias'], df[['Pó de Pedra', 'Pedrisco', 'Brita 1', 'Brita 2', 'Obs Secundário']]], axis=1)
 
     # Transformando o DataFrame para um formato longo
-    melted_line2_df = line2_df.melt(id_vars=['Dias'], value_vars=['Pó de Pedra', 'Pedrisco', 'Brita 1', 'Brita 2'], 
+    melted_line2_df = line2_df.melt(id_vars=['Dias', 'Obs Secundário'], value_vars=['Pó de Pedra', 'Pedrisco', 'Brita 1', 'Brita 2'], 
                                     var_name='Material', value_name='Produção (ton.)')
 
     # Verifica o DataFrame resultante
@@ -170,7 +204,8 @@ def update_charts(selected_month, selected_unit):
         color='Material',
         title=f'Produção Diária - {selected_month}', 
         line_shape='linear', 
-        color_discrete_map=color_map
+        color_discrete_map=color_map,
+        hover_data={'Obs Secundário': True}
     )
     fig_line2.update_layout(
         xaxis_title='Período',
@@ -181,25 +216,29 @@ def update_charts(selected_month, selected_unit):
     # Gráfico de linha para USA e USS
     if selected_unit == 'USA':
         usa_uss_data = filtered_df.melt(id_vars=['Dias'], value_vars=['CBUQ', 'Binder'], var_name='Material', value_name='Produção (ton.)')
+        usa_uss_data['Obs USA'] = df['Obs USA']
         fig_usa_uss = px.line(
             usa_uss_data,
             x='Dias',
             y='Produção (ton.)',
             color='Material',
             title=f'Produção Diária - USA {selected_month}',
-            labels={'value': 'Produção (ton.)', 'variable': 'Material'}
+            labels={'value': 'Produção (ton.)', 'variable': 'Material'},
+            hover_data={'Obs USA': True}
         )
         fig_usa_uss.update_traces(line=dict(color='#006699'), selector=dict(name='CBUQ'))
         fig_usa_uss.update_traces(line=dict(color='#FFCC00'), selector=dict(name='Binder'))
     else:
         usa_uss_data = filtered_df.melt(id_vars=['Dias'], value_vars=['BGS', 'BGTC'], var_name='Material', value_name='Produção (ton.)')
+        usa_uss_data['Obs USS'] = df['Obs USS']
         fig_usa_uss = px.line(
             usa_uss_data,
             x='Dias',
             y='Produção (ton.)',
             color='Material',
             title=f'Produção Diária - USS {selected_month}',
-            labels={'value': 'Produção (ton.)', 'variable': 'Material'}
+            labels={'value': 'Produção (ton.)', 'variable': 'Material'},
+            hover_data={'Obs USS': True}
         )
         fig_usa_uss.update_traces(line=dict(color='#006699'), selector=dict(name='BGS'))
         fig_usa_uss.update_traces(line=dict(color='#FFCC00'), selector=dict(name='BGTC'))
@@ -209,7 +248,7 @@ def update_charts(selected_month, selected_unit):
         legend_font_size=14
     )
     
-    return fig_line, fig_bar, fig_pie, fig_line2, fig_usa_uss
+    return fig_line, fig_acum, fig_bar, fig_pie, fig_line2, fig_usa_uss
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8050))
